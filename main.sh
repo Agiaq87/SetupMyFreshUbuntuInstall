87b5dae6cddad481a30cd358dfed27f1f7e4b351
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2025-06-03 10:12:50 +0200

    correction

--- main.sh
+++ main.sh
@@ -7,18 +7,21 @@ source "$SCRIPT_DIR/common/utils.sh"
 
 #check_root
 
-if [ -f /etc/arch-release ]; then
-    log WARN "Found an Arch Linux installation"
-    ./arch/install.sh
-    ./arch/config.sh
-elif [ -f /etc/lsb-release ]; then
+if [ -f /etc/lsb-release ]; then
     log "Found an Ubuntu installation"
     ./common/setup_bash.sh
     ./ubuntu/base.sh
-    .ubuntu/develop.sh
+    ./ubuntu/develop.sh
     ./ubuntu/docker.sh
     ./ubuntu/latex.sh
     ./ubuntu/mobile.sh
     ./ubuntu/synology.sh
     ./ubuntu/config.sh
 fi
+
+
+#if [ -f /etc/arch-release ]; then
+#    log WARN "Found an Arch Linux installation"
+#    ./arch/install.sh
+#    ./arch/config.sh
+#el
